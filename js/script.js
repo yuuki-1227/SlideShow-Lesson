160ebe04d5588cffa490b89fde4288e86d2929dc
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-20 09:26:23 +0900

    [Upsate]スライドショー

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition : 'fade',
+  speed : 1000,
+  easing : 'easeOutQuart',
+  navType : 'block',
+  childrenElementType : 'div',
+  arrows : true,
+  autoPlay : false,
+  autoPlayDuration : 5000,
+  keyboardOnAlways : true,
+  hidePrevious : false
+});
